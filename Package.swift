dfbc00df42a70753d5f780cdc7905f9b4e1d9fbd
Anka <runner@Mac-1715174538753.local>
2024-05-08 13:44:42 +0000

    KMM SPM package release for 0.2.7

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/Airthings/KmpLog/com/airthings/lib/kmplog-kmmbridge/0.2.7/kmplog-kmmbridge-0.2.7.zip"
+let remoteKotlinChecksum = "efd940c2a5e16e194cbeaae75d92ad92fd69c753f8ef283e21a19302e1440945"
+let packageName = "KmpLog"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
