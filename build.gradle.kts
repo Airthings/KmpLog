ca74e7be8174cdb989d74e0ac8ac24ba946be58b
Eirik Vale Aase <e.v.aase@gmail.com>
2024-10-25 23:53:54 +0200

    Simplify KmmBridge publishing setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-private val version = (properties["AUTO_VERSION"] ?: properties["LIBRARY_VERSION"])?.toString()
-    ?: error("Fatal error: unable to determine the library version.")
-
 private val iosFrameworkName = "KmpLog"
 
 rootProject.group = "${properties["GROUP"]}"
-rootProject.version = version
+rootProject.version = "${properties["LIBRARY_VERSION"]}"
 
 buildscript {
     repositories {
